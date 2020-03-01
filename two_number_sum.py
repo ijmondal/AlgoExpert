226c1d8ac81fc665382e11603890dbf1e06b6e99
ijmondal <indrojyoti.m15@iiits.in>
2020-03-01 18:01:20 +0530

    Day 1

--- two_number_sum.py
+++ two_number_sum.py
@@ -9,6 +9,36 @@ def two_number_sum(array, targetSum):
     
     return []
 
+#O(n) time | O(n) space
+def two_number_sum2(array, targetSum):
+    hashMap = {}
+    
+    for num in array:
+        potentialMatch = targetSum - num
+        if potentialMatch in hashMap:
+            return [num, potentialMatch]
+        else:
+            hashMap[num] = True
+
+    return []
+
+#O(nlogn) time | O(1) space
+def two_number_sum3(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array)-1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right -= 1
+    
+    return []
+
+
 L = [5, 3, -4, 8, 11, 1, -1, 6]
 
-print(two_number_sum(L, 10))
+print(two_number_sum3(L, 10))
