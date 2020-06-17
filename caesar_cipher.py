322bf339610a8d08cf360d15ea07a09ac27ad373
ijmondal <indrojyoti.m15@iiits.in>
2020-06-17 23:05:28 +0530

    caesar cipher

---
+++ caesar_cipher.py
@@ -0,0 +1,12 @@
+def caesarCipher(string, key):
+    newLetters = []
+    newKey = key %26
+    for letter in string:
+        newLetters.append(getNewLetter(letter, newKey))
+    return "".join(newLetters)
+
+def getNewLetter(letter, newKey):
+    newLetterCode = ord(letter) + newKey
+    return chr(newLetterCode) if newLetterCode <=122 else chr(96 + newLetterCode % 122)
+
+print(caesarCipher("abcd", 5))
