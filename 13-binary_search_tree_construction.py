f1417f6d6a444531f3a8436f9e0c0e099c961a56
ijmondal <indrojyoti.m15@iiits.in>
2020-06-21 21:07:02 +0530

    bst

---
+++ 13-binary_search_tree_construction.py
@@ -0,0 +1,20 @@
+'''
+binary_search_tree insertion, deletion, searching
+'''
+class BST:
+    def __init__(self):
+        self.value = value
+        self.left = None
+        self.right = None
+    
+    def insert(self, value):
+        currentNode = self
+        while True:
+            
+
+    def search(self, value):
+        pass
+
+    def remove(self, value, parentNode = None):
+        pass
+
