d2062deb333d4c696efcea69980c0bd81f5698b7
ijmondal <indrojyoti.m15@iiits.in>
2020-06-23 19:57:15 +0530

    bst

--- 13-binary_search_tree_construction.py
+++ 13-binary_search_tree_construction.py
@@ -7,14 +7,78 @@ class BST:
         self.left = None
         self.right = None
     
+    # Average: O(logn) time| O(1) space
+    # Worst: O(m) time/ O(1) space
     def insert(self, value):
         currentNode = self
         while True:
+            if value < currentNode.value:
+                if currentNode.left is None:
+                    currentNode.left = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.left
+            else:
+                if currentNode.right is None:
+                    currentNode.right = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.right
+        return self
+
             
+     # Average: O(logn) time| O(1) space
+    # Worst: O(m) time/ O(1) space
+    def contains(self, value):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                currentNode = currentNode.right
+            else:
+                return True
+        return False
 
-    def search(self, value):
-        pass
-
+ # Average: O(logn) time| O(1) space
+    # Worst: O(m) time/ O(1) space
     def remove(self, value, parentNode = None):
-        pass
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.right
+            else:
+                # three subcases
+                if currentNode.left is not None and currentNode.right is not None:
+                    currentNode.value = currentNode.right.getMinValue()
+                    # currentNode.value  = smallest value of right subtree
+                    currentNode.right.remove(currentNode.value, currentNode)
+                # we're gonna back to this root node case
+                elif parentNode is None:
+                    if currentNode.left is not None:
+                        currentNode.value = currentNode.left.value
+                        currentNode.right = currentNode.left.right
+                        currentNode.left = currentNode.left.left
+                    elif currentNode.right is not None:
+                        currentNode.value = currentNode.right.value
+                        currentNode.left = currentNode.right.left
+                        currentNode.right = currentNode.right.right
+                    else:
+                        currentNode.value = None
+                        
+                elif parentNode.left == currentNode:
+                    parentNode.left = currentNode.left if currentNode.left is not None else currentNode.right
+                elif parentNode.right == currentNode:
+                    parentNode.right = currentNode.left if currentNode.left is not None else currentNode.right
+    
+    def getMinValue(self):
+        currentNode = self
+        while currentNode.left is not None:
+            currentNode = currentNode.left
+        return currentNode.value
+                    
 
