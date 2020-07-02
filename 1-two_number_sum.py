7f9eb4bf55dfc342840fbfb515b1d5bce1396f2f
ijmondal <indrojyoti.m15@iiits.in>
2020-07-02 22:06:57 +0530

    two number sum

--- 1-two_number_sum.py
+++ 1-two_number_sum.py
@@ -1,3 +1,18 @@
+'''
+## Two Number Sum
+
+#### Problem Statement
+
+Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. If any two numbers in the input array sum up to the
+target sum, the function should return them in an array, in sorted order. If no two numbers sum up to the target sum, the function should return an empty array.
+Assume that there will be at most one pair of numbers summing up to the target sum.
+
+`Sample input: [3, 5, -4, 8, 11, 1, -1, 6], 10
+Sample output: [-1, 11]`
+
+'''
+
+
 # O(n^2) time | O(1) space
 def two_number_sum(array, targetSum):
     for i in range(len(array)-1):
