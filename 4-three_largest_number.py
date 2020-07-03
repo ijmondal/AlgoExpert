d2ebe87ce929d1eeda83836c1ff533ae76ef6bab
ijmondal <indrojyoti.m15@iiits.in>
2020-07-03 22:46:47 +0530

    three_largest_num

--- 4-three_largest_number.py
+++ 4-three_largest_number.py
@@ -1,3 +1,15 @@
+'''
+## Find Three Largest Numbers
+
+#### Problem Statement
+
+
+Write a function that takes in an array of integers and returns a sorted array of the three largest integers in the input array. Note that the function should return
+duplicate integers if necessary; for example, it should return [10, 10, 12] for an input array of [10, 5, 9, 10, 12].
+Sample input: [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
+Sample output: [18, 141, 541]
+
+'''
 # O(n) Time | O(1) space
 def find_three_largest_numbers(arr):
     three_largest = [None]*3
